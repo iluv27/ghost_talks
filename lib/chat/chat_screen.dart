c7af459999dc52d3fe139c5c93605a36d0400d97
iluv27 <ehiedujoy277@gmail.com>
2023-01-22 05:54:20 +0100

    new commit

--- lib/chat/chat_screen.dart
+++ lib/chat/chat_screen.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
 
-class ButtonExample extends StatelessWidget {
-  const ButtonExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      borderRadius: BorderRadius.circular(15),
-      onTap: () {},
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        child: const Text("Button"),
-      ),
-    );
-  }
-}
