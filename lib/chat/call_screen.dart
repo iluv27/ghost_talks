8371c3c7ee2f1fba883098ce9d45de88b9b69b25
iluv27 <ehiedujoy277@gmail.com>
2023-02-11 22:26:04 +0100

    new commit

--- lib/chat/call_screen.dart
+++ lib/chat/call_screen.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+class CallScreen extends StatelessWidget {
+  const CallScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Center(
+        child: Container(
+      child: const Text('Hi Calls'),
+    ));
+  }
+}
---
+++ lib/chat/profile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Center(
+        // ignore: avoid_unnecessary_containers
+        child: Container(
+      child: const Text('Hi Profile'),
+    ));
+  }
+}
